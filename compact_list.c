a1d90a461036f4d847c330472fc32d6a1a64095b
Jicheng Zhi <jzhi001@fiu.edu>
2019-02-20 01:26:20 +0800

    finish basic functions of CompactList

--- compact_list.c
+++ compact_list.c
@@ -1,74 +1,111 @@
 
 #include <string.h>
 #include <assert.h>
+#include <math.h>
+#include <stdlib.h>
 #include "integer.h"
 #include "compact_list.h"
 #include "panic.h"
 
+#define COMPACT_LIST_DEBUG
+#ifdef COMPACT_LIST_DEBUG
+
+static void show_bytes(CompactList *list) {
+    unsigned char *pt = (unsigned char *) list;
+    printf("bytes: ");
+    for (int i = 0; i < 8; i++) {
+        printf("%2X ", pt[i]);
+    }
+    printf("\nsize: ");
+
+    pt += 8;
+    for (int i = 0; i < 4; i++) {
+        printf("%2X ", pt[i]);
+    }
+    printf("\nelements: ");
+    pt += 4;
+
+    for (int i = 12; i < list->bytes - 1; i++) {
+        printf("%2X ", pt[i - 12]);
+    }
+
+    printf("\nend: %02X\n", ((char *) list)[list->bytes - 1]);
+}
+
+#endif
+
+/**
+ * Used for constructing an entry.
+ */
+typedef struct {
+    char type;
+    unsigned char encoding;
+
+    char len[8];
+    uint8_t sizeofLen;
+
+    char *data;
+    uint32_t sizeofData;
+
+    char total[10];
+    int sizeofTotal;
+} CompactListNode;
+
 inline int64_t CompactListSize(CompactList *list) {
     return list->size;
+}
+
+static inline char *cl_getEndOfList(CompactList *list) {
+    return ((char *) list) + list->bytes - 1;
 }
 
 static inline size_t cl_headerBytes() {
     return sizeof(CompactList);
 }
 
-static inline size_t cl_emptyListBytes() {
+static inline size_t cl_sizeofEmptyList() {
     return cl_headerBytes() + CL_END_BYTES;
 }
 
 
 CompactList *CompactListNew() {
     CompactList *list;
-    if ((list = malloc(cl_emptyListBytes())) == NULL) {
+    if ((list = malloc(cl_sizeofEmptyList())) == NULL) {
         panic("CompactList malloc failed\n");
     }
     list->size = 0;
-    list->bytes = cl_emptyListBytes();
+    list->bytes = cl_sizeofEmptyList();
     ((char *) list)[cl_headerBytes()] = (char) CL_END;
     return list;
 }
 
+inline void CompactListFree(CompactList *list) {
+    free(list);
+}
+
 static int cl_getEntryType(char encoding) {
-    return encoding >> 6;
+    return (unsigned char) encoding >> 6;
 }
 
-#define SET_INT_ENC_AND_DATA(intType) do { \
-    enc = CL_INT ## intType; \
-    int ## intType ##_t *pt = (int ## intType ##_t*)(encoding + 1); \
-    pt[0] = (int ## intType ##_t)val; \
-    len += INT ## intType ## _BYTES; \
-} while(0);
-
-static inline size_t cl_setIntEncodingAndData(int64_t val, char *encoding) {
-    assert(encoding);
-
-    uint8_t bits = bitCount(val);
-    char enc;
-    size_t len = 1;
-
-    if (bits <= 4) {
-        enc = (char) (CL_INT4 | val);
-    } else if (bits <= 8 * INT8_BYTES) {
-        SET_INT_ENC_AND_DATA(8);
-    } else if (bits <= 8 * INT16_BYTES) {
-        SET_INT_ENC_AND_DATA(16);
-    } else if (bits <= 8 * INT32_BYTES) {
-        SET_INT_ENC_AND_DATA(32);
-    } else {
-        SET_INT_ENC_AND_DATA(64);
-    }
-    encoding[0] = enc;
-    return len;
+static inline int cl_isIntEntry(const char *ele) {
+    return cl_getEntryType(ele[0]) == CL_TYPE_INT;
 }
 
-static void cl_strNode_getEncoding(CompactListNode *node) {
+static inline int cl_isStrEntry(const char *ele) {
+    return cl_getEntryType(ele[0]) == CL_TYPE_STR;
+}
+
+static inline int cl_isEndOfList(const char *ele) {
+    return ele[0] == (char) CL_END;
+}
+
+static void cl_strNode_setEncoding(CompactListNode *node) {
     assert(node->type == CL_TYPE_STR);
-    size_t dataLen = node->sizeofData;
+    int64_t dataLen = node->sizeofData;
     uint8_t dataLenbits = bitCount(dataLen);
 
     if (dataLenbits <= 4) {
-        node->encoding = (char) (CL_STR4 | dataLen);
+        node->encoding = (unsigned char) (CL_STR4 | dataLen);
     } else if (dataLenbits <= 8 * INT8_BYTES) {
         node->encoding = CL_STR8;
         node->sizeofLen = (uint8_t) int_setValue(node->len, (int64_t) dataLen);
@@ -81,44 +118,234 @@ static void cl_strNode_getEncoding(CompactListNode *node) {
     }
 }
 
-static void cl_intNode_getEncoding(CompactListNode *node, int64_t val){
+
+static void cl_intNode_setEncodingAndData(CompactListNode *node, int64_t val) {
     assert(node->type == CL_TYPE_INT);
 
     uint8_t bytes = bytesForInt(val), bits = bitCount(val);
     if (bits <= 4) {
-        node->encoding = (char) (CL_INT4 | val);
+        node->encoding = (unsigned char) (CL_INT4 | val);
         node->sizeofData = 0;
-    } else{
+    } else {
         char *intVal;
-        if((intVal = malloc(bytes)) == NULL){
+        if ((intVal = malloc(bytes)) == NULL) {
             panic("CompactList malloc for int val node failed\n");
         }
         int_setValue(intVal, val);
         node->data = intVal;
         node->sizeofData = bytes;
+        switch (bytes) {
+            case INT8_BYTES:
+                node->encoding = CL_INT8;
+                break;
+            case INT16_BYTES:
+                node->encoding = CL_INT16;
+                break;
+            case INT32_BYTES:
+                node->encoding = CL_INT32;
+                break;
+            case INT64_BYTES:
+                node->encoding = CL_INT64;
+                break;
+            default:
+                panic("CompactList unknown int type %d\n", bytes);
+        }
     }
 }
 
 
-static inline void cl_node_getTotal(CompactListNode *node) {
-    uint32_t encDataSize = node->sizeofLen + node->sizeofData + 1;
-    uint32_t total = bytesForUnsignedInt(encDataSize) + encDataSize;
-    node->sizeofTotal = bytesForUnsignedInt(total);
+//size used to store data length
+static uint32_t cl_getDataLenSize(const char *ele) {
+    unsigned char enc = (unsigned char) ele[0];
+    switch (enc & 0xF0) {
+        case CL_STR4:
+        case CL_INT4:
+        case CL_INT8:
+        case CL_INT16:
+        case CL_INT32:
+        case CL_INT64:
+            return 0;
+        case CL_STR8:
+            return 1;
+        case CL_STR16:
+            return 2;
+        case CL_STR32:
+            return 4;
+        default:
+            panic("CompactList getDataLenSize: unknown entry encoding %d\n", enc & 0xF0);
+    }
+}
 
-    //split [total] and pack bits with mask
+//get data size
+static inline uint32_t cl_getDataSize(char *ele) {
+    unsigned char enc = (unsigned char) ele[0];
+    unsigned char type = (unsigned char) (enc & 0xF0);
+
+    if (type == CL_INT4) {
+        return 0;
+    } else if (type == CL_STR4) {
+        return (uint32_t) (enc & 0x0F);
+    } else if (cl_isIntEntry(ele)) {
+        //TODO refactor
+        if (type == CL_INT8) {
+            return 1;
+        } else if (type == CL_INT16) {
+            return 2;
+        } else if (enc == CL_INT32) {
+            return 4;
+        } else if (enc == CL_INT64) {
+            return 8;
+        } else {
+            goto err;
+        }
+    } else if (cl_isStrEntry(ele)) {
+        uint32_t dataLenSize = cl_getDataLenSize(ele);
+        return (uint32_t) int_getValue(ele + 1, dataLenSize);
+    } else {
+        goto err;
+    }
+
+    err:
+    panic("CompactList getDataSize: unknown string entry encoding 0x%x\n", enc & 0xF0);
+}
+
+//get total size
+static inline uint32_t cl_getTotalSize(char *ele) {
+    uint32_t tot = cl_getDataLenSize(ele) + CL_ENC_BYTES + cl_getDataSize(ele);
+    uint32_t totBits = bitCount(tot);
+    return totBits / 7 + (totBits % 7 > 0 ? 1 : 0);
+}
+
+
+static inline void cl_node_setTotal(CompactListNode *node) {
+    uint32_t encDataSize = node->sizeofLen + node->sizeofData + CL_ENC_BYTES;
+    uint32_t encDataBits = bitCount(encDataSize);
+
+    uint32_t sizeofTotal = encDataBits / 7 + (encDataBits % 7 > 0 ? 1 : 0);
+    node->sizeofTotal = sizeofTotal;
+
     for (int i = node->sizeofTotal - 1; i >= 0; i--) {
-        //get 6 least significant bits
-        char cell = (char) (CL_ENTRY_TOTAL | (total & 0x3F));
-        node->total[i] = cell;
-        total >>= 6;
+        char part = (char) (encDataSize & 0x7F);
+        if (i == 0) {
+            part |= 0x80;
+        }
+        node->total[i] = part;
+        encDataSize >>= 7;
     }
 }
 
-CompactListNode *cl_node_new(){
-    CompactListNode *node;
-    if((node = malloc(sizeof(*node))) == NULL){
-        panic("CompactList malloc node failed\n");
+static inline void cl_ensureNode(const char *ele) {
+    char enc = ele[0];
+    if (enc != (char) CL_END) {
+        char type = (char) cl_getEntryType(enc);
+        assert(type == CL_TYPE_STR || type == CL_TYPE_INT);
     }
+}
+
+static inline uint32_t cl_getEntrySize(char *ele) {
+    return CL_ENC_BYTES + cl_getDataLenSize(ele) + cl_getDataSize(ele) + cl_getTotalSize(ele);
+}
+
+static char *cl_nextElement(char *ele) {
+    cl_ensureNode(ele);
+    if (ele[0] == (char) CL_END) {
+        return NULL;
+    } else {
+        return ele + cl_getEntrySize(ele);
+    }
+}
+
+//idx is index of ele
+static char *cl_prevElement(char *ele, int64_t idx) {
+    if (idx - 1 < 0) return NULL;
+    cl_ensureNode(ele);
+
+    char *pt = ele - 1;
+    unsigned char totPart, weight = 0;
+    uint64_t entrySize = 0;
+    int stop = 0;
+    do {
+        totPart = (unsigned char) (pt[0]);
+        entrySize |= (totPart & 0x7F) << (7 * weight);
+        weight++;
+        pt--;
+        stop = totPart >> 7;
+    } while (!stop);
+    return pt - entrySize + 1;
+}
+
+static char *cl_firstElement(CompactList *list) {
+    return list->size > 0 ? (char *) list + cl_headerBytes() : NULL;
+}
+
+static char *cl_lastElement(CompactList *list) {
+    if (list->size == 0) {
+        return NULL;
+    } else {
+        char *end = (char *) list + list->bytes - 1;
+        return cl_prevElement(end, list->size);
+    }
+}
+
+static char *cl_elementAt(CompactList *list, int64_t idx) {
+    assert(list->size > 0 && idx >= 0 && idx < list->size);
+    char *node;
+
+    if (idx <= list->size / 2) {
+        //iter from head
+        node = cl_firstElement(list);
+        while (idx > 0) {
+            node = cl_nextElement(node);
+            idx--;
+        }
+    } else {
+        //iter from tail
+        node = cl_lastElement(list);
+        int64_t curIdx = list->size - 1;
+        while (curIdx > idx) {
+            node = cl_prevElement(node, curIdx);
+            curIdx--;
+        }
+    }
+
+    return node;
+}
+
+//return -1 if int val, 0 or positive number if string val
+static int64_t cl_valueAt(CompactList *list, int64_t idx, int64_t *intVal, char **strVal) {
+    char *ele = cl_elementAt(list, idx);
+    unsigned char enc = (unsigned char) ele[0];
+    unsigned char type = (unsigned char) cl_getEntryType(enc);
+    uint32_t dataLenSize = cl_getDataLenSize(ele);
+
+    if (type == CL_TYPE_STR) {
+        if ((enc & 0xF0) == CL_STR4) {
+            if (strVal) *strVal = ele + 1;
+            return ele[0] & 0x0F;
+        } else {
+            if (strVal) *strVal = ele + CL_ENC_BYTES + dataLenSize;
+            return int_getValue(ele + 1, dataLenSize);
+        }
+    } else if (type == CL_TYPE_INT) {
+        //TODO refactor encoding
+        if (enc & 0xF0 == CL_INT4) {
+            if (intVal) *intVal = ele[0] & 0x0F;
+        } else if (enc == CL_INT64) {
+            if (intVal) *intVal = int_getValue(ele + 1, INT64_BYTES);
+        } else if ((enc & 0xF0) == CL_INT16) {
+            if (intVal) *intVal = int_getValue(ele + 1, INT16_BYTES);
+        } else if ((enc & 0xF0) == CL_INT32) {
+            if (intVal) *intVal = int_getValue(ele + 1, INT32_BYTES);
+        } else {
+            panic("CompactList valueAt: unknown encoding 0x%x\n", enc);
+        }
+        return -1;
+    } else {
+        panic("CompactList valueAt: unknown entry type 0x%x\n", type);
+    }
+}
+
+static inline CompactListNode *cl_node_init(CompactListNode *node) {
     node->data = NULL;
     node->sizeofData = 0;
     node->sizeofLen = 0;
@@ -126,26 +353,169 @@ CompactListNode *cl_node_new(){
     return node;
 }
 
-void cl_node_free(CompactListNode *node){
-    free(node->data);
-    free(node);
+static inline uint32_t cl_node_size(CompactListNode *node) {
+    return CL_ENC_BYTES + node->sizeofLen + node->sizeofData + node->sizeofTotal;
 }
 
-//TODO add tail now
-CompactList *CompactListAdd(CompactList *list, char *data, size_t dataLen) {
-    int64_t val;
-    char encoding[24];
-
-    if (string2int(data, dataLen, &val) == 1) {
-
-        //TODO realloc, shift right, memcpy
-    } else {
-        cl_setStringEncodingAndLen(dataLen, encoding);
-
+int64_t CompactListIndexOf(CompactList *list, char *data, size_t len){
+    if(list->size == 0){
+        return -1;
     }
 
+    uint32_t idx = 0;
+    char *strVal;
+    int64_t intVal;
+    while (idx < list->size ) {
+        int64_t entryDataLen = cl_valueAt(list, idx, &intVal, &strVal);
+        if (entryDataLen == -1) {
+            int64_t idata;
+            int succ = string2int(data, len, &idata);
+            if (succ && (idata == intVal)) {
+                return idx;
+            }
+        } else {
+            if ((entryDataLen == len) && (strncmp(strVal, data, len) == 0)) {
+                return idx;
+            }
+        }
+        idx++;
+    }
+    return -1;
+}
 
+//TODO getValue(entry)
+CompactList *CompactListRemove(CompactList *list, char *data, size_t len, int *ret) {
+    int64_t idx = CompactListIndexOf(list, data, len);
+    if(idx == -1){
+        if(ret) *ret = 0;
+        return list;
+    }
+
+    if(ret) *ret = 1;
+    char *tar = cl_elementAt(list, idx);
+    //shift left
+    uint32_t entrySize = cl_getEntrySize(tar);
+    int64_t cpyLen = cl_getEndOfList(list) - (tar + entrySize) + 1;
+    memcpy(tar, tar + entrySize, (size_t)cpyLen);
+
+    list->bytes -= entrySize;
+    list->size--;
+    if((list = realloc(list, list->bytes)) == NULL){
+        panic("CompactList remove: realloc failed\n");
+    }
+
+    return list;
+
+}
+
+CompactList *CompactListInsert(CompactList *list, char *data, size_t dataLen, int64_t idx) {
+    if (list->size == UINT32_MAX) {
+        panic("CompactList list is full\n");
+    } else if (idx > list->size) {
+        panic("index too big, max index is list size\n");
+    }
+    //create node
+    int64_t val;
+    CompactListNode node;
+    cl_node_init(&node);
+    node.sizeofData = (uint32_t) dataLen;
+    node.data = data;
+
+    if (string2int(data, dataLen, &val) == 1) {
+        node.type = CL_TYPE_INT;
+        cl_intNode_setEncodingAndData(&node, val);
+    } else {
+        node.type = CL_TYPE_STR;
+        cl_strNode_setEncoding(&node);
+    }
+    cl_node_setTotal(&node);
+
+    //resize
+    if ((list = realloc(list, list->bytes + cl_node_size(&node))) == NULL) {
+        panic("CompactList realloc failed\n");
+    }
+
+    //shift right
+    char *ele;
+    if (idx == list->size) {
+        //add at tail
+        ele = cl_getEndOfList(list);
+        list->bytes += cl_node_size(&node);
+        char *end = cl_getEndOfList(list);
+        end[0] = (char) CL_END;
+    } else {
+        char *start = cl_elementAt(list, idx);
+        char *end = cl_getEndOfList(list);
+        ele = start;
+        memcpy(start + cl_node_size(&node), start, end - start + 1);
+        list->bytes += cl_node_size(&node);
+    }
     list->size++;
-    //TODO update list->bytes
+
+    //insert node
+    ele[0] = node.encoding;
+    ele++;
+    if (node.sizeofLen > 0) {
+        memcpy(ele, node.len, node.sizeofLen);
+        ele += node.sizeofLen;
+    }
+
+    if (node.sizeofData > 0) {
+        memcpy(ele, node.data, node.sizeofData);
+    }
+    ele += node.sizeofData;
+
+    memcpy(ele, node.total, (size_t) node.sizeofTotal);
+    if (node.type == CL_TYPE_INT) free(node.data);
     return list;
 }
+
+
+#define COMPACT_LIST_TEST
+#ifdef COMPACT_LIST_TEST
+
+int main() {
+    CompactList *list = CompactListNew();
+
+    char *strVal;
+    int64_t intVal, ret;
+    int rmRet;
+
+    list = CompactListInsert(list, "hello", 5, 0);
+    ret = cl_valueAt(list, 0, &intVal, &strVal);
+    assert(ret == 5);
+    assert(strncmp(strVal, "hello", (size_t) ret) == 0);
+
+
+    list = CompactListInsert(list, "world", 5, 0);
+    ret = cl_valueAt(list, 0, &intVal, &strVal);
+    assert(ret >= 0);
+    assert(strncmp(strVal, "world", (size_t) ret) == 0);
+
+    list = CompactListInsert(list, "12345", 5, 1);
+    ret = cl_valueAt(list, 1, &intVal, &strVal);
+    assert(ret == -1);
+    assert(intVal == 12345);
+
+    list = CompactListInsert(list, "9223372036854775807", 19, 2);
+    ret = cl_valueAt(list, 2, &intVal, &strVal);
+    assert(ret == -1);
+    assert(intVal == INT64_MAX);
+
+    char *str = "AAAAAAAAAAAAAAAAAAAAAAAA";
+    list = CompactListInsert(list, str, strlen(str), 0);
+    ret = cl_valueAt(list, 0, &intVal, &strVal);
+    assert(ret == 24);
+    assert(strncmp(strVal, str, strlen(str)) == 0);
+
+
+    show_bytes(list);
+    list = CompactListRemove(list, "12345", 5, &rmRet);
+    assert(rmRet == 1);
+    show_bytes(list);
+
+    CompactListFree(list);
+    return 0;
+}
+
+#endif
--- compact_list.h
+++ compact_list.h
@@ -11,20 +11,21 @@
  * [cl-bytes] [size] [entry] ... [entry] [cl-end]
  * entry: [encoding & data-len] [data] [entry-bytes]
  */
-typedef struct {
+typedef struct __attribute__((__packed__)){
     uint64_t bytes; //bytes used of the list and entries
     uint32_t size; //element count
 } CompactList;
 
-#define CL_END 0xFF;
-#define CL_END_BYTES 1;
+#define CL_ENC_BYTES 1
+#define CL_END 0xFF
+#define CL_END_BYTES 1
 
 #define CL_TYPE_INT 1
 #define CL_INT4 0x40
 #define CL_INT8 0x50
 #define CL_INT16 0x60
 #define CL_INT32 0x70
-#define CL_INT64 0x80
+#define CL_INT64 0x7F
 
 #define CL_TYPE_STR 0
 #define CL_STR4 0x00
@@ -32,7 +33,6 @@ typedef struct {
 #define CL_STR16 0x20
 #define CL_STR32 0x34
 
-#define CL_ENTRY_TOTAL 0x70
 
 /**
  * Encoding:
@@ -44,31 +44,21 @@ typedef struct {
  * [00 10 0000] [len] string16
  * [00 11 0100] [len] * 4 string32
  *
- * [01 00 data] int4
- * [01 01 0000] [data] int8
- * [01 10 0000] [data] * 2 int16
- * [01 01 0000] [data] * 4 int32
- * [01 11 0000] [data] * 8 int64
+ * [01 1 data] int4
+ * [01 0 00001] [data] int8
+ * [01 0 00010] [data] * 2 int16
+ * [01 0 00100] [data] * 4 int32
+ * [01 0 01000] [data] * 8 int64
  *
- * total:
- * [10 xxxxxx] [10 xxxxxx] ...
+ * total: this pattern is used for prevElement(),
+ * we calculate entry size directly when calling next()
+ *
+ * [1 xxxxxxx] [0 xxxxxxx] ...
  */
-typedef struct {
-    char type;
-    char encoding;
-
-    char len[8];
-    uint8_t sizeofLen;
-
-    char *data;
-    uint32_t sizeofData;
-
-    char total[10];
-    int sizeofTotal;
-} CompactListNode;
 
 
 CompactList *CompactListNew();
+void CompactListFree(CompactList *list);
 
 int64_t CompactListSize(CompactList *list);
 
@@ -76,8 +66,10 @@ char *CompactListGetFirst(CompactList *list);
 
 char *CompactListGetLast(CompactList *list);
 
-CompactList *CompactListAdd(CompactList *list, char *data, size_t dataLen);
+CompactList *CompactListInsert(CompactList *list, char *data, size_t dataLen, int64_t idx);
 
-CompactList *CompactListRemove(CompactList *list, char *data, size_t len);
+CompactList *CompactListRemove(CompactList *list, char *data, size_t len, int *ret);
+
+int64_t CompactListIndexOf(CompactList *list, char *data, size_t len);
 
 #endif //COMPACT_LIST_H
